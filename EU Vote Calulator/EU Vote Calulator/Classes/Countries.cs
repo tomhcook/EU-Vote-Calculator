6e03936b0a8ce532f7af96cda113580475054f21
err0rz-404 <60301479+err0rz-404@users.noreply.github.com>
2020-02-07 11:36:07 +0000

    V 0.0.1
    Class For Countrie has been added

---
+++ EU Vote Calulator/EU Vote Calulator/Classes/Countries.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EU_Vote_Calulator.Classes
+{
+    class Countries
+    {
+        public string imgpath;
+        public string Name;
+        public bool Vote;
+        public float pop;
+        
+    }
+}
--- EU Vote Calulator/EU Vote Calulator/Form1.cs
+++ EU Vote Calulator/EU Vote Calulator/Form1.cs
@@ -15,6 +15,21 @@ namespace EU_Vote_Calulator
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            Classes.Countries twat = new Classes.Countries();
+            twat.Name = "asburger";
+            twat.pop = 22.6f;
+            twat.Vote = true;
+            twat.imgpath = @"C:\Users\computing\Downloads\ch0015_00_takeshi_256.png";
+            
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
         }
     }
 }
